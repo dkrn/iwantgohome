af3493dfb26588fad8fc53c1fa74e9f4f2fb7c75
dkrn <41932046+dkrn@users.noreply.github.com>
2018-11-29 10:44:35 +0900

    add

--- coffee/head.php
+++ coffee/head.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-    <title>Half Slider - Start Bootstrap Template</title>
+    <title>Coffee</title>
     <!-- Bootstrap core JavaScript -->
     <script src="vendor/jquery/jquery.min.js"></script>
     <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
