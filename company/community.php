0b9b8c6883fe6503288cebcb074ffe26ce3b82de
dkrn <41932046+dkrn@users.noreply.github.com>
2018-12-04 14:00:53 +0900

    final final update

--- company/community.php
+++ company/community.php
@@ -40,32 +40,55 @@
 					   </li>
 					</ul>
 				</div>
-				<div class="col-md-6 col-lg-3 p-0">
+				<div class="col-md-12 col-lg-6 event_wrap">
 					<div class="event text-light">
-						<h4 class="nsbold">진행중인 이벤트</h3>
+						<h4 class="nsbold pt-3">진행중인 이벤트</h4>
 						<p>바로가기 ></p>
+						<ul class="m-0">
+							<li>
+								<a href="#">Qahwah 12월 멤버스 이벤트</a>
+								<span>2018-12-01</span>
+							</li>
+							<li>
+								<a href="#">12월 따끈따끈 이벤트</a>
+								<span>2018-12-01</span>
+							</li>
+							<li>
+								<a href="#">행복 우체통 이벤트</a>
+								<span>2018-11-11</span>
+							</li>
+							<li>
+								<a href="#">Qahwah 11월 멤버스 이벤트</a>
+								<span>2018-11-01</span>
+							</li>
+							<li>
+								<a href="#">오싹오싹 헬로할로윈</a>
+								<span>2018-10-01</span>
+							</li>
+						</ul>
 					</div>
 				</div>
-				<div class="col-md-6 col-lg-3 p-0">
+				<div class="col-md-12 col-lg-6 p-0 bg-light">
+					<div class="inquiry">
+						<h3 class="nsbold text-center">1:1 문의</h3>
+					</div>
+				</div>
+				<div class="col-md-12 col-lg-6 p-0">
 					<div class="prize text-light">
-						<h4 class="nsbold">당첨자 발표</h3>
+						<h4 class="nsbold">당첨자 발표</h4>
 						<p>바로가기 ></p>
 					</div>
 				</div>
-			</div>
-			<div class="row">
-				<div class="col-md-12 col-lg-7 p-0">
+				<div class="col-md-12 col-lg-12 p-0">
 					<div class="qa">
 						<div>
 							<h3 class="nsbold text-light text-center">Q&A</h3>
 						</div>
 					</div>
 				</div>
-				<div class="col-md-12 col-lg-5 p-0 bg-light">
-					<div class="inquiry">
-						<h3 class="nsbold text-center">1:1 문의</h3>
-					</div>
-				</div>
+			</div>
+			<div class="row">
+				
 			</div>
 		</div>
 	</section>
