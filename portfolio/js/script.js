27fc75f8f97a1ab5dfa1f76259c4f0ca381c175b
dkrn <41932046+dkrn@users.noreply.github.com>
2018-12-27 15:17:34 +0900

    update

--- portfolio/js/script.js
+++ portfolio/js/script.js
@@ -12,7 +12,89 @@ function contact(){
 	$('#contact').css({display:'block'});
 }
 
+
+
 $(function(){
+
+	$('#fullpage').fullpage({
+        slidesNavigation: false
+	});
+
+	$('#fullpage-sub').fullpage({
+	    navigation: true,
+	    navigationTooltips: ['INCREDIBLES 2', 'Renaultsamsung', 'Qahwah', 'Web Design'],
+	    slidesNavigation: false,
+	    showActiveTooltip: true,
+	    scrollOverflow:true,
+	    afterLoad:function(origin,destination,direction){
+			if(destination.index == 0){
+				$('#page01').find('.main_cmt').css({
+					opacity:1,
+					'transition-duration':'1.5s',
+					transform:'translateX(0)'
+				});
+	        }else if(origin && origin.index == 0){
+	         	$('#page01').find('.main_cmt').removeAttr('style').css({
+	         		opacity:0,
+	         		transform:'translateX(-100px)'
+	         	});
+	        }
+
+	        if(destination.index == 1){
+				$('#page02').find('.main_cmt').css({
+					opacity:1,
+					'transition-duration':'1.5s',
+					transform:'translateX(0)',
+				});
+	        }else if(origin && origin.index == 1){
+	         	$('#page02').find('.main_cmt').removeAttr('style').css({
+	         		opacity:0,
+	         		transform:'translateX(-100px)'
+	         	});
+	        }
+
+	        if(destination.index == 2){
+				$('#page03').find('.main_cmt').css({
+					opacity:1,
+					'transition-duration':'1.5s',
+					transform:'translateX(0)',
+				});
+	        }else if(origin && origin.index == 2){
+	         	$('#page03').find('.main_cmt').removeAttr('style').css({
+	         		opacity:0,
+	         		transform:'translateX(-100px)'
+	         	});
+	        }
+
+	        if(destination.index == 3){
+				$('#page04').find('.main_cmt').css({
+					opacity:1,
+					'transition-duration':'1.5s',
+					transform:'translateX(0)',
+				});
+	        }else if(origin && origin.index == 3){
+	         	$('#page04').find('.main_cmt').removeAttr('style').css({
+	         		opacity:0,
+	         		transform:'translateX(-100px)'
+	         	});
+	        }
+		},
+		onLeave: function(origin, destination, direction){
+			var leavingSection = this;
+
+			if(destination.index != 1){
+				$('#fp-nav ul li a span,.fp-slidesNav ul li a span,#menu-open div').css({background:'#fff'});
+				$('#fp-nav ul li .fp-tooltip').css({color:'#fff'});
+				$('nav h1 a img').attr('src','./images/logo.png')
+			}else if(destination.index == 1){
+				$('#fp-nav ul li a span,.fp-slidesNav ul li a span,#menu-open div').css({background:'#000'});
+				$('#fp-nav ul li .fp-tooltip').css({color:'#000'});
+				$('nav h1 a img').attr('src','./images/logo_black.png')
+			}
+		}
+	});
+
+
 
 	$('#menu-open').on({
 		mouseenter:function(){
@@ -37,4 +119,13 @@ $(function(){
 			menuClose();
 		}
 	});
+
+	$('.bt-line').on({
+		mouseenter:function(){
+			$('.bt-line span').addClass('add');
+		},
+		mouseleave:function(){
+			$('.bt-line span').removeClass('add');
+		}
+	})
 });
